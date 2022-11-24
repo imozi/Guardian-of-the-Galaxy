f22c80f77bc5122e3bbc85cf849a7d0d210478ab
Anna Chernova <ashubina0810@mail.ru>
2022-11-24 17:01:25 +0300

    правка тестов

--- packages/client/src/components/App/App.test.tsx
+++ packages/client/src/components/App/App.test.tsx
@@ -1,7 +1,7 @@
 import App from './App'
 import { render, screen } from '@testing-library/react'
 
-const appContent = 'Вот тут будет жить ваше приложение :)'
+const appContent = 'Guardian of the Galaxy'
 
 // @ts-ignore
 global.fetch = jest.fn(() =>
@@ -10,5 +10,5 @@ global.fetch = jest.fn(() =>
 
 test('Example test', async () => {
   render(<App />)
-  expect(screen.getByText(appContent)).toBeDefined()
+  expect(screen.findByText(appContent)).toBeDefined()
 })
