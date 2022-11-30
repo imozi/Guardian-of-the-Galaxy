fe5a40160794c452a884c97dee5a6c7c85db1e5b
dzhal <djal.amninov@gmail.com>
2022-12-01 00:10:49 +0600

    feat: error boundary

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import './styles/index.scss'
 import { BrowserRouter } from 'react-router-dom'
+import ErrorBoundary from './utils/ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 )
