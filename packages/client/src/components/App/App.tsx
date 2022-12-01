1c5234768b1f885376e07088885e55fcef7b454d
Oleg Zaytsev <ZaytsevOI.90@gmail.com>
2022-12-01 18:13:15 +0300

    feat: добавляет страницу профиля, компонент аватарки  (#8)

--- packages/client/src/components/App/App.tsx
+++ packages/client/src/components/App/App.tsx
@@ -3,12 +3,14 @@ import { Branch } from '../../pages/Branch'
 import { Forum } from '../../pages/Forum'
 import { Login } from '../../pages/Login'
 import { NewMessage } from '../../pages/NewMessage'
+import { Profile } from '../../pages/Profile'
 import { Register } from '../../pages/Register'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/profile" element={<Profile />} />
       <Route path="/register" element={<Register />} />
       <Route path="/forum" element={<Forum />} />
       <Route path="/forum-branch" element={<Branch />} />
