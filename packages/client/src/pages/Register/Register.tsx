d6c076d4110cc8449f4a59662d04a7d939473822
Anna Chernova <ashubina0810@mail.ru>
2022-12-01 08:20:52 +0300

    feat: добавлена регистрация

--- packages/client/src/pages/Register/Register.tsx
+++ packages/client/src/pages/Register/Register.tsx
@@ -5,10 +5,40 @@ import { Card } from '../../components/Card'
 import { Layout } from '../../components/Layout'
 import { Link } from 'react-router-dom'
 import { isAxiosError } from 'axios'
-import { login } from '../../api/auth'
+import { register } from '../../api/auth'
 import { useForm } from '../../hooks/useForm'
 
-const loginForm = [
+const registerForm = [
+  {
+    name: 'first_name',
+    type: 'text',
+    label: 'First name',
+    value: '',
+    isValid: true,
+    rule: /^[А-ЯA-Z][а-яa-z-]+$/,
+    errorMessage:
+      'Latin or Cyrillic alphabet is allowed, the first letter must be capital, without spaces and without numbers, without special characters (only a hyphen is allowed)',
+  },
+  {
+    name: 'phone',
+    type: 'text',
+    label: 'Phone',
+    value: '',
+    isValid: true,
+    rule: /^(\+)?((\d{2,3}) ?\d|\d)(([ -]?\d)|( ?(\d{2,3}) ?)){9}\d$/,
+    errorMessage:
+      'The phone number must be from 10 to 15 characters, consist of numbers, may start with a plus',
+  },
+  {
+    name: 'second_name',
+    type: 'text',
+    label: 'Second name',
+    value: '',
+    isValid: true,
+    rule: /^[А-ЯA-Z][а-яa-z-]+$/,
+    errorMessage:
+      'Latin or Cyrillic alphabet is allowed, the first letter must be capital, without spaces and without numbers, without special characters (only a hyphen is allowed)',
+  },
   {
     name: 'login',
     type: 'text',
@@ -18,6 +48,16 @@ const loginForm = [
     rule: /^[a-zA-Z][a-zA-Z0-9_-]{2,20}$/,
     errorMessage:
       'Login must be from 3 to 20 characters, Latin, may contain numbers, but not consist of them, without spaces, without special characters (hyphens and underscores are allowed)',
+  },
+  {
+    name: 'email',
+    type: 'email',
+    label: 'Email',
+    value: '',
+    isValid: true,
+    rule: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
+    errorMessage:
+      'Email must be in Latin, it can include numbers and special characters like a hyphen, there must be a “dog” (@) and a dot after it, but there must be letters before the dot',
   },
   {
     name: 'password',
@@ -31,8 +71,8 @@ const loginForm = [
   },
 ]
 
-export const Login = () => {
-  const [formValues, isFormValid, formInputs] = useForm(loginForm)
+export const Register = () => {
+  const [formValues, isFormValid, formInputs] = useForm(registerForm)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState('')
 
@@ -46,7 +86,7 @@ export const Login = () => {
       setLoading(true)
 
       try {
-        await login(formData as AuthDTO)
+        await register(formData as AuthDTO)
         setError('')
       } catch (e) {
         if (isAxiosError(e)) {
@@ -60,20 +100,20 @@ export const Login = () => {
 
   return (
     <Layout>
-      <div className="login">
+      <div className="register">
         <Card title="Guardian of the Galaxy">
           <form className="form" onSubmit={onSubmit}>
             <>{formInputs()}</>
             {!!error && <div className="form__error">{error}</div>}
             <div className="form__footer">
               <Button type="submit" loading={loading}>
-                Sign in
+                Sign up
               </Button>
             </div>
           </form>
           <div className="card__footer">
-            <Link className="link" to="/register">
-              Sign up
+            <Link className="link" to="/">
+              Sign in
             </Link>
           </div>
         </Card>
