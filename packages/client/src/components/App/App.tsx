92afcdeb93d99657b0f93f359e4b2c476bff91df
AnyaChernova <40361915+AnyaChernova@users.noreply.github.com>
2022-12-04 18:53:32 +0300

    Task/w2ts-19 раздел со сменой пароля (#15)
    feat: добавлен раздел со сменой пароля

--- packages/client/src/components/App/App.tsx
+++ packages/client/src/components/App/App.tsx
@@ -4,6 +4,7 @@ import { Forum } from '../../pages/Forum'
 import { Game } from '../../pages/Game'
 import { Login } from '../../pages/Login'
 import { NewMessage } from '../../pages/NewMessage'
+import { Password } from '../../pages/Password'
 import { Profile } from '../../pages/Profile'
 import { Register } from '../../pages/Register'
 
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/password" element={<Password />} />
       <Route path="/register" element={<Register />} />
       <Route path="/forum" element={<Forum />} />
       <Route path="/forum-branch" element={<Branch />} />
