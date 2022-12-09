d9c010c655657ff651a8c8fe4ca24feb5c34105f
putiniyama <85397886+putiniyama@users.noreply.github.com>
2022-12-09 20:32:37 +0200

    feat: добавлен Fullscreen API при нажатии на клавишу "f"

---
+++ packages/client/src/webAPI/fullscreen.ts
@@ -0,0 +1,13 @@
+export function fullscreenAPI() {document.addEventListener("keydown", (e) => {
+  if (e.key === "f") {
+    toggleFullScreen();
+  }
+}, false);}
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
+  }
+}
