9022914ef4be1924246d4125a7f4dbb085fc7248
AnyaChernova <40361915+AnyaChernova@users.noreply.github.com>
2022-12-16 07:57:14 +0300

    feat: добавлены класс персонажа и движение персонажа (#19)
    feat: добавлены класс персонажа и движение персонажа

---
+++ packages/client/src/model/MainShip.ts
@@ -0,0 +1,145 @@
+import { Gif } from '../utils/gif'
+import { SHIP_ENTITY } from '../utils/consts'
+import { rotateObject } from '../utils/game'
+
+export default class MainShip {
+  private _rightPressed = false
+  private _leftPressed = false
+  private _upPressed = false
+  private _downPressed = false
+  private _rotation = SHIP_ENTITY.rotation
+  private readonly _canvasWidth: number
+  private readonly _canvasHeight: number
+  private readonly _velocity: number
+  private readonly _width: number
+  private readonly _height: number
+  private _x: number
+  private _y: number
+  private _ship: Gif
+  private _shipLeft: Gif
+  private _shipRight: Gif
+
+  constructor(canvasWidth: number, canvasHeight: number) {
+    this._canvasWidth = canvasWidth
+    this._canvasHeight = canvasHeight
+    this._velocity = SHIP_ENTITY.velocity
+
+    this._x = this._canvasWidth / 2
+    this._y = this._canvasHeight - SHIP_ENTITY.bottomIdent
+    this._width = SHIP_ENTITY.width
+    this._height = SHIP_ENTITY.height
+    this._ship = new Gif({ url: SHIP_ENTITY.src })
+    this._shipLeft = new Gif({ url: SHIP_ENTITY.srcLeft })
+    this._shipRight = new Gif({ url: SHIP_ENTITY.srcRight })
+
+    document.addEventListener('keydown', this._onKeydown)
+    document.addEventListener('keyup', this._onKeyup)
+  }
+
+  public draw(ctx: CanvasRenderingContext2D) {
+    this._move()
+    this._checkWalls()
+
+    const { _x, _y, _width, _height } = this
+
+    const drawImageParams: [number, number, number, number] = [
+      _x,
+      _y,
+      _width,
+      _height,
+    ]
+    const rotateParams: [number, number] = [_x + _width / 2, _y + _height / 2]
+
+    if (this._rightPressed) {
+      rotateObject(ctx, this._rotation, ...rotateParams)
+      ctx.drawImage(this._shipRight.image, ...drawImageParams)
+    } else if (this._leftPressed) {
+      rotateObject(ctx, -this._rotation, ...rotateParams)
+      ctx.drawImage(this._shipLeft.image, ...drawImageParams)
+    } else {
+      ctx.drawImage(this._ship.image, ...drawImageParams)
+    }
+  }
+
+  private _checkWalls(): void {
+    const widthDiff = this._canvasWidth - this._width
+    const heightDiff = this._canvasHeight - this._height
+    switch (true) {
+      // left
+      case this._x < 0:
+        this._x = 0
+        break
+      // up
+      case this._y < 0:
+        this._y = 0
+        break
+      // right
+      case this._x > widthDiff:
+        this._x = widthDiff
+        break
+      // down
+      case this._y > heightDiff:
+        this._y = heightDiff
+        break
+    }
+  }
+
+  private _move(): void {
+    switch (true) {
+      case this._rightPressed:
+        this._x += this._velocity
+        break
+      case this._leftPressed:
+        this._x += -this._velocity
+        break
+      case this._upPressed:
+        this._y += -this._velocity
+        break
+      case this._downPressed:
+        this._y += this._velocity
+        break
+    }
+  }
+
+  private _onKeydown = (event: KeyboardEvent): void => {
+    switch (event.code) {
+      case 'ArrowRight':
+      case 'KeyD':
+        this._rightPressed = true
+        break
+      case 'ArrowLeft':
+      case 'KeyA':
+        this._leftPressed = true
+        break
+      case 'ArrowUp':
+      case 'KeyW':
+        this._upPressed = true
+        break
+      case 'ArrowDown':
+      case 'KeyS':
+        this._downPressed = true
+        break
+    }
+  }
+
+  private _onKeyup = (event: KeyboardEvent): void => {
+    switch (event.code) {
+      case 'ArrowRight':
+      case 'KeyD':
+        this._rightPressed = false
+        break
+      case 'ArrowLeft':
+      case 'KeyA':
+        this._leftPressed = false
+        break
+      case 'ArrowUp':
+      case 'KeyW':
+        this._upPressed = false
+        break
+      case 'ArrowDown':
+      case 'KeyS':
+        this._downPressed = false
+        break
+    }
+  }
+}
--- packages/client/src/pages/Game/Game.tsx
+++ packages/client/src/pages/Game/Game.tsx
@@ -1,11 +1,40 @@
-import { Canvas } from '../../components/Canvas'
+import React, { useEffect, useRef, useState } from 'react'
 import { Layout } from '../../components/Layout'
-import React from 'react'
+import MainShip from '../../model/MainShip'
 
 export const Game = () => {
+  const boardIndent = 32
+  const boardHeight = window.innerHeight - boardIndent
+  const boardWidth = window.innerWidth - boardIndent
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const mainShip = new MainShip(boardWidth, boardHeight)
+
+  useEffect(() => {
+    animate()
+  }, [])
+
+  const animate = () => {
+    if (!canvasRef.current) {
+      return
+    }
+
+    canvasRef.current.width = boardWidth
+    canvasRef.current.height = boardHeight
+
+    const context = canvasRef.current && canvasRef.current.getContext('2d')
+    if (context) {
+      context.clearRect(0, 0, boardWidth, boardHeight)
+      mainShip.draw(context)
+      context.restore()
+      context.save()
+    }
+
+    requestAnimationFrame(animate)
+  }
+
   return (
     <Layout isGame={true}>
-      <Canvas id="game" />
+      <canvas ref={canvasRef} />
     </Layout>
   )
 }
