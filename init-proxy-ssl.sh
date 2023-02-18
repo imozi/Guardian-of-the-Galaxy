de41d6fdbfa85bec82b3f3186b1106a57dabfcfd
Oleg Zaytsev <zaytsevoi.90@gmail.com>
2023-02-18 22:37:37 +1200

    fix: изменяет конфиг docker-compose.prod

--- init-proxy-ssl.sh
+++ init-proxy-ssl.sh
@@ -9,7 +9,7 @@ fi
 
 domains=(example.com)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./reverse-proxy/data/certbot"
 email="example@mail.com"
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
